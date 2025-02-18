4ab877e0ee21e98e20924c282dd2520f00a470d7
DonDonny-0 <brand.t2004@icloud.com>
2025-02-18 19:49:01 +0000

    CREATE backend

---
+++ models/Task.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"],
+        trim: true,
+        maxlength: [20, "maximum characters reached (20)"]
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = mongoose.model('Task', taskSchema)
